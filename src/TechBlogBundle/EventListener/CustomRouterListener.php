10cdca73e6e80aa8e595a76eb8e2f5b432c40297
Aleks Sushitskiy <magnetos.ua@gmail.com>
2019-03-01 20:46:14 +0200

    Create Listener for redirecting if url not exist

---
+++ src/TechBlogBundle/EventListener/CustomRouterListener.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace TechBlogBundle\EventListener;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\RouterInterface;
+
+/**
+ * Change template name from FrameworkExtraBundle TemplateGuesser.
+ * Becouse TemplateGuesser convert to lowercase name of views folders
+ *
+ * (for example: in DefaultController appropriate view template is located in Resources/views/Default/ folder)
+ *
+ * Class MyTemplateNameFixer
+ * @package TechBlogBundle\EventListener
+ */
+class CustomRouterListener implements EventSubscriberInterface
+{
+    /**
+     * @var Container
+     */
+    private $container;
+
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $router = $this->container->get('router');
+
+        /** @var Request $request */
+        $request = $event->getRequest();
+
+        if (preg_match_all('/^twig.controller.exception/', $request->get('_controller'))) {
+            $url = $router->generate('tech_blog_homepage');
+            $response = new RedirectResponse($url);
+            $event->setResponse($response);
+        }
+    }
+
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => ['onKernelRequest'],
+        ];
+    }
+}
+
